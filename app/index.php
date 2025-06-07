b3cddbca5fe9c9826479b092dcb0de538fcd9093
Sangram Kishor Moharana <42498676+itzSaan@users.noreply.github.com>
2025-06-07 23:57:21 +0530

    Update index.php

--- app/index.php
+++ app/index.php
@@ -1,4 +1,440 @@
-<?php
-// Enter your code here, enjoy!
+<?php require('include/header.php');?>
 
-    echo "Hello World";
+<body class="index-page">
+    <?php require('include/navbar.php');?>
+    
+    <main class="main">
+      <!-- Hero Section -->
+      <section id="hero" class="hero section">
+        <img
+          id="heroImage"
+          src="assets/img/hero-bg.jpg"
+          alt=""
+          data-aos="fade-in"
+        />
+
+        <div class="container">
+          <div class="row">
+            <div class="d-flex flex-column align-items-bottom">
+              <div class="hero-welcome">Welcome to WiseTech ✨</div>
+              <!-- <h2 data-aos="fade-up" data-aos-delay="100" id="heroText">
+                <span>Discover</span> the Power of Innovation.
+              </h2> -->
+              <h2 id="heroText">
+              Discover the Power of Innovation
+              </h2>
+              <p data-aos="fade-up" data-aos-delay="200">
+                At WiseTech, we are dedicated to transforming the future of
+                technology. Our mission is to innovate and deliver cutting-edge
+                technology solutions that empower businesses and individuals to
+                achieve their full potential. Join us on our journey to shape
+                tomorrow, today.
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- /Hero Section -->
+      
+      <section id="getStarted" class="get-started">
+        <div class="container">
+          <div class="row mb-5" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-sm-6">
+              <div>
+                <p class="section-title">Get Started with us</p>
+              <h2 class="section-heading">
+                Get started with us <br />
+                simply and quickly
+              </h2>
+              </div>
+            </div>
+            <div class="col-auto ms-lg-auto mt-auto">
+              <div>
+                <a href="contact.html" class="btn section-btn">
+                  <span class="btn-content">Start with us</span>
+                  <span class="material-symbols-outlined btn-content"> arrow_forward </span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="row g-4 align-items-stretch">
+            <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="100">
+              <div class="get-started-card">
+                <a href="register.php" class="">
+                  <span class="material-symbols-outlined"> Face </span>
+                  <div>
+                    <h5>Create Account</h5>
+                    <p>
+                      Users must create an account to access various services on
+                      Nitro.
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="200">
+              <div class="get-started-card" >
+                <a href="service.php" class="streched-link">
+                  <span class="material-symbols-outlined"> analytics </span>
+                  <div>
+                    <h5>Explore the Website</h5>
+                    <p>
+                      Learn about the agency's services, projects, team, and
+                      values.
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-12" data-aos="fade-up" data-aos-delay="300">
+              <div class="get-started-card" >
+                <a href="contact.php" class="">
+                  <span class="material-symbols-outlined"> person </span>
+                  <div>
+                    <h5>Request a Quote</h5>
+                    <p>
+                      Looking for a specific service, then can request a quote
+                      from the agency.
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End of get started section -->
+
+      <!-- approach section -->
+      <section class="approach">
+        <div class="container py-4">
+          <p class="section-title" data-aos="fade-up" data-aos-delay="100">Our Approach</p>
+          <h3 data-aos="fade-up" data-aos-delay="100">
+            To be a global leader in technology innovation, driving progress and
+            transforming industries through our solutions.
+          </h3>
+        </div>
+      </section>
+
+      <!-- End of approach section -->
+
+      <!-- Vision and Mission section -->
+      <section class="vision-and-mission">
+        <div class="container">
+          <div class="row mb-5" data-aos="fade-up" data-aos-delay="100">
+            <div class="col-sm-12 col-lg-8">
+              <p class="section-title">Our Vision & Mission</p>
+              <h2 class="section-heading">
+                Present Creative and <br />
+                innovative solutions.
+              </h2>
+            </div>
+            <div class="col col-auto ms-lg-auto me-md-aut mt-auto">
+     
+            <a href="about.php" class="section-btn ms-auto">
+              <span class="btn-content">All About WiseTech</span>
+              <span class="material-symbols-outlined btn-content"> arrow_forward </span>
+            </a>
+
+            </div>
+          </div>
+
+          <div class="row gap-4">
+            <div class="col-sm-12" data-aos="fade-up" data-aos-delay="100">
+              <div class="goal-card">
+                <p>Our Goals</p>
+              <h3>To be the Leading agency in the industry.</h3>
+              </div>
+            </div>
+            <div class="col">
+              <div class="mission-card">
+                <p>Our Mission</p>
+                <h3>Customer Success <br />is our Mission.</h3>
+                <span
+                >We prioritize your growth by delivering innovative, tailored solutions that ensure long-term success and business excellence.</span
+                >
+              </div>
+            </div>
+            <div class="col" data-aos="fade-up" data-aos-delay="200">
+              <div class="vision-card">
+                <p>Our Vision</p>
+                <h3>
+                  We are working <br />
+                  on Future.
+                </h3>
+                <span>WiseTech innovates future-ready solutions, shaping industries with cutting-edge technology for smarter, scalable businesses.</span
+                >
+              </div>
+            </div>
+            <div class="col-sm-12" data-aos="fade-up" data-aos-delay="100">
+             <div class="values-card">
+              <p>Our Values</p>
+              <h3>
+                The Way <br />
+                we are.
+              </h3>
+              <div class="row g-3 overflow-hidden" data-aos="fade-up" data-aos-delay="100">
+                <div class="col-lg col-sm-6">
+                  <div class="col">
+                    <span
+                    class="material-symbols-outlined fs-1 p-2 rounded-circle text-bg-dark lead mb-3"
+                    >
+                    lightbulb
+                  </span>
+                  <h4>Innovation</h4>
+                  <span
+                  >Continuously pushing the boundaries of technology.</span
+                  >
+                </div>
+                </div>
+                <div class="col-lg col-sm-6">
+                  <div class="col">                    
+                    <span
+                    class="material-symbols-outlined fs-1 p-2 rounded-circle text-bg-dark lead mb-3"
+                    >
+                    star
+                  </span>
+                  <h4>Integrity</h4>
+                  <span
+                  >Upholding the highest standards of honesty and
+                  transparency.</span
+                  >
+                </div>
+                </div>
+                <div class="col-lg col-sm-6">
+                  <div class="col">
+                    <span
+                    class="material-symbols-outlined fs-1 p-2 rounded-circle text-bg-dark lead mb-3"
+                  >
+                    workspace_premium
+                  </span>
+                  <h4>Excellence</h4>
+                  <span>Striving for perfection in everything we do.</span>
+                  </div>
+                </div>
+                <div class="col-lg col-sm-6">
+                  <div class="col">
+                    <span
+                    class="material-symbols-outlined fs-1 p-2 rounded-circle text-bg-dark lead mb-3"
+                  >
+                    groups
+                  </span>
+                  <h4>Collaboration</h4>
+                  <span>Working together to achieve common goals.</span>
+                  </div>
+                </div>
+                <div class="col-lg col-sm-12">
+                  <div class="col">
+                    <span
+                    class="material-symbols-outlined fs-1 p-2 rounded-circle text-bg-dark lead mb-3"
+                  >
+                    target
+                  </span>
+                  <h4>Customer Focus</h4>
+                  <span
+                    >Prioritizing the needs and success of our clients.</span>
+                  </div>
+                </div>
+                </div>
+             </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--  End of vision & mission section -->
+      <!-- Our Portfolio Section -->
+      <section id="portfolio" class="portfolio">
+        <div class="container">
+          <div class="text-center mb-4">
+            <h2 class="section-heading" data-aos="fade-up" data-aos-delay="100">See our Portfolio.</h2>
+          </div>
+          <!-- Accordion -->
+          <div class="accordion accordion-flush" id="accordionFlushExample">
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseOne"
+                >
+                  <span>01</span>
+                  <h2>UI/UX Design & Websites</h2>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseOne"
+                class="accordion-collapse collapse"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div class="accordion-body row gy-4">
+                  <div class="col-lg-12 col-sm-12">                    
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Eaque, esse quisquam. Aut fugiat praesentium dolor nulla
+                      accusamus ipsa perspiciatis, ipsam laudantium pariatur..
+                    </p>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <a href="#" class="img-fluid gap-3">
+                      <img
+                      src="./assets/img/goal-image.webp"
+                      class="img-fluid"
+                      height="100"
+                      alt=""
+                      />
+                    </a>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <a href="#" class="d-flex gap-3">
+                      <img
+                      src="./assets/img/goal-image.webp"
+                      class="img-fluid"
+                      height="100"
+                      alt=""
+                      />
+                    </a>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <a href="#" class="d-flex gap-3">
+                      <img
+                      src="./assets/img/goal-image.webp"
+                      class="img-fluid"
+                      height="100"
+                      alt=""
+                      />
+                    </a>
+                  </div>
+                  <div class="col-lg-3 col-sm-6">
+                    <a href="#" class="d-flex gap-3">
+                      <img
+                      src="./assets/img/goal-image.webp"
+                      class="img-fluid"
+                      height="100"
+                      alt=""
+                      />
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseTwo"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseTwo"
+                >
+                  <span>02</span>
+                  <h2>Web App Development</h2>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseTwo"
+                class="accordion-collapse collapse"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div class="accordion-body">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque, esse quisquam. Aut fugiat praesentium dolor nulla
+                    accusamus ipsa perspiciatis, ipsam laudantium pariatur..
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div class="accordion-item">
+              <h2 class="accordion-header">
+                <button
+                  class="accordion-button collapsed"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseThree"
+                  aria-expanded="false"
+                  aria-controls="flush-collapseThree"
+                >
+                  <span>03</span>
+                  <h2>Business Solutions</h2>
+                </button>
+              </h2>
+              <div
+                id="flush-collapseThree"
+                class="accordion-collapse collapse"
+                data-bs-parent="#accordionFlushExample"
+              >
+                <div class="accordion-body">
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Eaque, esse quisquam. Aut fugiat praesentium dolor nulla
+                    accusamus ipsa perspiciatis, ipsam laudantium pariatur..
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End of Portfolio Section -->
+
+      <!-- Review Section -->
+      <section class="testimonials">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-7">
+              <div class="p-md-4 p-2">
+              <p class="section-title pt-3 ps-3">By Our Clients</p>
+              <!-- Swiper -->
+              <div class="swiper testimonial-swiper" data-aos="fade-up" data-aos-delay="100">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide d-flex align-items-center justify-content-between">
+                        <div class="py-5 px-3">
+                            <h5><i class="bi bi-quote"></i>Wisetech delivered an outstanding website that exceeded our expectations. Highly recommend their professional and creative team!
+                              <i style="rotate: y -180deg; display: inline-block;" class="bi bi-quote" ></i>
+                            </h5>
+                            <h6>Sangram Kishor</h6>
+                            <span>CEO, NgiX Inc.</span>
+                        </div>                        
+                  </div>
+                  <div class="swiper-slide d-flex align-items-center justify-content-between">
+                    <div class="py-5 px-3">
+                        <h5><i class="bi bi-quote"></i>Impressed with Wisetech's custom app development. They turned our vision into a seamless, user-friendly application. Top-notch service!
+                          <i style="rotate: y -180deg; display: inline-block;" class="bi bi-quote" ></i>
+                        </h5>
+                        <h6>Sitansu Sekhar</h6>
+                        <span>MD, SocialMan</span>
+                    </div>                        
+              </div>
+              <div class="swiper-slide d-flex align-items-center justify-content-between">
+                <div class="py-5 px-3">
+                    <h5><i class="bi bi-quote"></i>Wisetech's web app solutions are innovative and efficient. Their expertise and support made our project a success. Will definitely work with them again!
+                      <i style="rotate: y -180deg; display: inline-block;" class="bi bi-quote" ></i>
+                    </h5>
+                    <h6>Roshan Pal</h6>
+                    <span>CEO, Tech Right Inc.</span>
+                </div>                        
+          </div>
+                </div>
+                <div class="swiper-button-next"></div>
+                <div class="swiper-button-prev"></div>
+              </div>
+            </div>
+          </div>
+            <div class="col-lg-5 col-md-8 ms-lg-auto mx-auto mt-auto" data-aos="fade-up" data-aos-delay="200">
+              <div class="text-center mt-auto w-100">
+                <img src="./assets/img/advertise-man-min.png" height="640" height="auto" class="img-fluid w-100 mt-auto" alt="business-woman">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!-- End of Review Section -->
+    </main>
+    
+    <?php require('include/footer.php');?>
+    
